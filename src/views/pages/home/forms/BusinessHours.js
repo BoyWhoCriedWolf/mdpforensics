822d8abe6c9c5bcdf8937c5b82049f1e42003896
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-09-11 01:19:05 +0800

    fix: photo styles

--- src/views/pages/home/forms/BusinessHours.js
+++ src/views/pages/home/forms/BusinessHours.js
@@ -8,10 +8,10 @@ export default function BusinessHours() {
   return (
     <div className="text-secondary">
       <Grid container>
-        <Grid item lg={6} md={6} sm={12} xs={12}>
+        <Grid item lg={4} md={4} sm={12} xs={12}>
           <img className="" src={IMG_PROFILE} alt="Michael" />
         </Grid>
-        <Grid item lg={6} md={6} sm={12} xs={12}>
+        <Grid item lg={8} md={8} sm={12} xs={12}>
           <CustomTypo variant="h4">
             Let MDP Forensics handle your claim preparation
           </CustomTypo>
